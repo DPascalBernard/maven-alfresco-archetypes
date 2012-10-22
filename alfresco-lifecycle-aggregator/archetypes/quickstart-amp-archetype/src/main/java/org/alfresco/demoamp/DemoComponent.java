90060a7158e63658bb6296e5567bcd4c63a9975f
columbro <columbro@04253f4f-3451-0410-a141-5562f1e59037>
2012-10-22 00:52:52 +0000

    -- added support for unit/integration testing using Maven classpath -- now running 'mvn test' cause src/test/java unit tests to be run. Classpath is built with Maven transitive dependencies from Alfresco POMs (tested against 4.2.b-SNAPSHOT POMs at https://artifacts.alfresco.com/nexus/content/groups/public-snapshots/org/alfresco/) -- added sample DemoComponent tests case, wiring with Foundation API. Green lights :) -- tested that mvn test and mvn jetty run-exploded work happily together -- refactored sample projects to include src/test/properties/<env> convention -- added support for test logging. added sample test-log4j.properties in src/test/resources/alfresco/extension

--- alfresco-lifecycle-aggregator/archetypes/quickstart-amp-archetype/src/main/java/org/alfresco/demoamp/DemoComponent.java
+++ alfresco-lifecycle-aggregator/archetypes/quickstart-amp-archetype/src/main/java/org/alfresco/demoamp/DemoComponent.java
@@ -16,26 +16,72 @@
  */
 package org.alfresco.demoamp;
 
-import java.util.logging.Logger;
-
+import org.alfresco.model.ContentModel;
 import org.alfresco.repo.module.AbstractModuleComponent;
+import org.alfresco.repo.nodelocator.NodeLocatorService;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.cmr.repository.StoreRef;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
  * A basic component that will be started for this module.
+ * Uses the NodeLocatorService to easily find nodes and the 
+ * NodeService to display them
  * 
- * @author Derek Hulley
+ * @author Gabriele Columbro
+ * @author Maurizio Pillitu
  */
 public class DemoComponent extends AbstractModuleComponent
 {
-	Log log = LogFactory.getLog(DemoComponent.class);
-	
+    private static final String COMPANY_HOME = "cm:companyhome";
+
+    Log log = LogFactory.getLog(DemoComponent.class);
+    
+    private NodeService nodeService;
+    
+    private NodeLocatorService nodeLocatorService;
+
+    public void setNodeService(NodeService nodeService) {
+        this.nodeService = nodeService;
+    }
+    
+    public void setNodeLocatorService(NodeLocatorService nodeLocatorService) {
+        this.nodeLocatorService = nodeLocatorService;
+    }
+
+    /**
+     * Bogus component execution 
+     */
     @Override
     protected void executeInternal() throws Throwable
     {
         System.out.println("DemoComponent has been executed");
-        log.debug("Test debug logging is working");
-        log.info("This should not be outputted by default");
+        log.debug("Test debug logging. Congratulation your AMP is working");
+        log.info("This is only for information purposed. Better remove me from the log in Production");
+    }
+    
+    /**
+     * This is a demo service interaction with Alfresco Foundation API.
+     * This sample method returns the number of child nodes of a certain type 
+     * under a certain node.
+     * 
+     * @return
+     */
+    public int childNodesCount(NodeRef nodeRef)
+    {
+        return nodeService.countChildAssocs(nodeRef, true);
+    }
+    
+    /**
+     * Returns the NodeRef of "Company Home"
+     * 
+     * @return
+     */
+    public NodeRef getCompanyHome()
+
+    {
+        return nodeLocatorService.getNode("companyhome", null, null);
     }
 }
